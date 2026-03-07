62a3d3ca6fca1c299fbc9183df8bd14b4fb850ab
maha16694-svg <maha16694@gmail.com>
2026-03-07 05:46:52 +0530

    Update hosting.py

--- tourism_project/hosting/hosting.py
+++ tourism_project/hosting/hosting.py
@@ -5,7 +5,7 @@ api = HfApi(token=os.getenv("HF_TOKEN"))
 
 api.upload_folder(
     folder_path="tourism_project/deployment",   # folder containing Dockerfile, app.py, requirements.txt
-    repo_id="maha16694-svg/tourism-package",    # your Hugging Face Space repo
+    repo_id="maha16694/tourism-package",    # your Hugging Face Space repo
     repo_type="space",
     path_in_repo=""
 )
